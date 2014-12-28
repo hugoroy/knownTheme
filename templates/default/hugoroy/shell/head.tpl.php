fbc868807e342d27174e879c63b7c617b1a6a56b
Hugo Roy <hugo@fsfe.org>
2014-12-28 16:35:15 +0100

    do not override the default css color so apply the custom background to a lower container

--- templates/default/hugoroy/shell/head.tpl.php
+++ templates/default/hugoroy/shell/head.tpl.php
@@ -1,6 +1,6 @@
 <link href="<?= \Idno\Core\site()->config()->getDisplayURL() ?>Themes/Hugoroy/css/default.css" rel="stylesheet">
 <style>
-    body {
+    .page-container {
         background: url(<?=Themes\Hugoroy\Controller::getBackgroundImageURL()?>) center fixed;
         background-size: cover;
     }
