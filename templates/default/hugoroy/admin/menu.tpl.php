253884728ad79c256f56c27c551be6f1e08539e2
Hugo Roy <hugo@fsfe.org>
2014-12-28 16:13:10 +0100

    update name, testing

---
+++ templates/default/hugoroy/admin/menu.tpl.php
@@ -0,0 +1,1 @@
+<li <?php if ($_SERVER['REQUEST_URI'] == '/admin/hugoroy/') echo 'class="active"'; ?>><a href="<?=\Idno\Core\site()->config()->getDisplayURL()?>admin/hugoroy/">Hugoroy Theme Options</a></li>
---
+++ templates/default/hugoroy/shell/head.tpl.php
@@ -0,0 +1,7 @@
+<link href="<?= \Idno\Core\site()->config()->getDisplayURL() ?>Themes/Hugoroy/css/default.css" rel="stylesheet">
+<style>
+    body {
+        background: url(<?=Themes\Hugoroy\Controller::getBackgroundImageURL()?>) center fixed;
+        background-size: cover;
+    }
+</style>
