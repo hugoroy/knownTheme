5aa810dc5496bb893fc2a6f9c7ef660b4bb38ee6
Hugo Roy <hugo@fsfe.org>
2014-12-28 16:39:11 +0100

    apply it on body

--- templates/default/hugoroy/shell/head.tpl.php
+++ templates/default/hugoroy/shell/head.tpl.php
@@ -1,7 +1,9 @@
 <link href="<?= \Idno\Core\site()->config()->getDisplayURL() ?>Themes/Hugoroy/css/default.css" rel="stylesheet">
 <style>
-    .page-container {
-        background: url(<?=Themes\Hugoroy\Controller::getBackgroundImageURL()?>) center fixed;
+    body {
+        background-image: url(<?=Themes\Hugoroy\Controller::getBackgroundImageURL()?>);
+        background-position: center;
+        background-attachment: fixed;
         background-size: cover;
     }
 </style>
